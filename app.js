1d69e4794c7068206c360d06d15640908388ee7c
sslav1 <71025978+sslav1@users.noreply.github.com>
2023-09-24 06:20:16 +0800

    Update app.js

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ app.get("/start", (req, res) => {
 });
 
 app.get("/nezha", (req, res) => {
-  let cmdStr = "/bin/bash nezha.sh server.abc.tk 5555 dfzPfEOagGDCAVhM4s >/dev/null 2>&1 &";
+  let cmdStr = "/bin/bash nezha.sh nezha.sslav.eu.org 5555 gGKNydjQEkJVERNiHD >/dev/null 2>&1 &";
   exec(cmdStr, function (err, stdout, stderr) {
     if (err) {
       res.send("哪吒客户端部署错误：" + err);
